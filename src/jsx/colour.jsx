3150f15426734fcd55773893d32f982da948d4a8
Shadows666 <lifeloveregretsxe@gmail.com>
2016-08-25 14:25:52 +0100

    Build display page in full. Include complete colour display options

--- src/jsx/colour.jsx
+++ src/jsx/colour.jsx
@@ -14,15 +14,23 @@ class Colour extends React.Component{
           <BackArrow palette={{backarrow:'#a0a2a3'}}/>
           <p className="select-colour">Select which colour you’d like to use:</p>
         </div>
-        //Rectangle
-        <p>Blue</p>
-        //Rectangle
-        <p>Green</p>
-        //Rectangle
-        <p>Pink</p>
-        //Rectangle
-        <p>Purple</p>
-        <div className="bottombar">
+        <div className="blue-box">
+          <div className="blue-square"></div>
+          <p className="colour-text">Blue</p>
+        </div>
+        <div className="green-box">
+          <div className="green-square"></div>
+          <p className="colour-text">Green</p>
+        </div>
+        <div className="pink-box">
+          <div className="pink-square"></div>
+          <p className="colour-text">Pink</p>
+        </div>
+        <div className="purple-box">
+          <div className="purple-square"></div>
+          <p className="colour-text">Purple</p>
+        </div>
+          <div className="bottombar">
           <BottomBar/>
         </div>
       </div>
